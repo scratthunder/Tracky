efdf3a0696e7b13d05aa4fb525e195fdd9fd2494
scratthunder <e_guggenbichler@gmx.at>
2012-09-22 13:52:49 +0200

    blubb

---
+++ Tracky/Tracky/Tracky.h
@@ -0,0 +1,15 @@
+//
+//  Tracky.h
+//  Tracky
+//
+//  Created by Edwin Guggenbichler on 9/22/12.
+//  Copyright (c) 2012 GraftInc. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+@interface Tracky : NSPreferencePane
+
+- (void)mainViewDidLoad;
+
+@end
---
+++ Tracky/Tracky/Tracky.m
@@ -0,0 +1,17 @@
+//
+//  Tracky.m
+//  Tracky
+//
+//  Created by Edwin Guggenbichler on 9/22/12.
+//  Copyright (c) 2012 GraftInc. All rights reserved.
+//
+
+#import "Tracky.h"
+
+@implementation Tracky
+
+- (void)mainViewDidLoad
+{
+}
+
+@end
